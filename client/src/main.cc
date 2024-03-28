efb72a7d6ff419886cd7b67e89ddb9c69dbac224
fomgleb <fomglib@gmail.com>
2024-03-28 10:11:21 +0300

    Make a little program to test opencv

--- client/src/main.cc
+++ client/src/main.cc
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-#include "client/inc/Recognizer.h"
-#include "client/inc/Saver.h"
-
-namespace client = table_recognizer::client;
-
+using namespace cv;
 int main() {
-  client::Recognizer recognizer;
-  recognizer.Capture();
-  client::Table recognized_table = recognizer.Recognize();
+  std::cout << "Test" << std::endl;
 
-  client::Saver saver;
-  saver.Save(recognized_table);
+  VideoCapture cap(0);  // open camera
+  if (!cap.isOpened())  // check if we succeeded
+    return 1;
+  Mat edges;
+  namedWindow("edges", 1);
+  while (1) {
+    Mat frame;
+    cap >> frame; // get a new frame from camera
+    cvtColor(frame, edges, COLOR_BGR2GRAY); // transform to black and white
+    GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);  // blur
+    Canny(edges, edges, 0, 30, 3);  // highlight edges
+    imshow("edges", edges);
+    if (waitKey(30) >= 0) break;
+  }
 
   return 0;
 }
