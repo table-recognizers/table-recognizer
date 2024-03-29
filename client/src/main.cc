5c25f3c24b44d2696bc140ee958388f16e155229
fomgleb <fomglib@gmail.com>
2024-03-29 21:59:24 +0300

    Remove `using namespace cv` from main.cc in client

--- client/src/main.cc
+++ client/src/main.cc
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
 int main() {
   std::cout << "Test" << std::endl;
 
-  VideoCapture cap(0);  // open camera
-  if (!cap.isOpened())  // check if we succeeded
+  cv::VideoCapture cap(0);  // open camera
+  if (!cap.isOpened())      // check if we succeeded
     return 1;
-  Mat edges;
-  namedWindow("edges", 1);
+  cv::Mat edges;
+  cv::namedWindow("edges", 1);
   while (1) {
-    Mat frame;
-    cap >> frame; // get a new frame from camera
-    cvtColor(frame, edges, COLOR_BGR2GRAY); // transform to black and white
-    GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);  // blur
-    Canny(edges, edges, 0, 30, 3);  // highlight edges
-    imshow("edges", edges);
-    if (waitKey(30) >= 0) break;
+    cv::Mat frame;
+    cap >> frame;  // get a new frame from camera
+    cv::cvtColor(frame, edges,
+                 cv::COLOR_BGR2GRAY);  // transform to black and white
+    cv::GaussianBlur(edges, edges, cv::Size(7, 7), 1.5, 1.5);  // blur
+    cv::Canny(edges, edges, 0, 30, 3);  // highlight edges
+    cv::imshow("edges", edges);
+    if (cv::waitKey(30) >= 0) break;
   }
 
   return 0;
