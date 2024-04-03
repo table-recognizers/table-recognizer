c9e1692df9050dddc061f891a3c80b05393f75a8
fomgleb <fomglib@gmail.com>
2024-04-03 15:48:34 +0300

    Refactor client/ImageLoader.cc

--- client/src/ImageLoader.cc
+++ client/src/ImageLoader.cc
@@ -1,12 +1,10 @@
 #include "client/inc/ImageLoader.h"
-namespace table_recognizer::client {
 
-// namespace Table = table_recognizer::client::Table;
-// std::string path1 = "C:\\Users\\grigo\\Downloads\\test";
+namespace table_recognizer::client {
 
 cv::Mat LoadImage(std::string path) {
   cv::Mat image = cv::imread(path);
   return image;
-}  // namespace table_recognizer::client::Table
+}
 
 }  // namespace table_recognizer::client
