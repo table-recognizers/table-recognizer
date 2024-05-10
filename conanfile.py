99be99680c0e526ef78001a5199dbc6d67b6d877
fomgleb <fomglib@gmail.com>
2024-05-10 14:00:24 +0300

    Add opencv lib using conan2

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+import os
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMake
+
+class FuckingShit(ConanFile):
+    generators = "CMakeDeps", "CMakeToolchain"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires("opencv/4.9.0")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
