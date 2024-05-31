cadc8e63dde6a319aaaea07eb8793ca1c9562487
fomgleb <fomglib@gmail.com>
2024-05-31 21:59:06 +0300

    Add xlsx file creation and filling with recognized data

--- client/main.cc
+++ client/main.cc
@@ -1,3 +1,4 @@
+#include "OpenXLSX/OpenXLSX.hpp"
 #include "client/application.h"
 #include "client/image_processing/table_detector.h"
 
@@ -14,12 +15,23 @@ int main() {
 
   cv::Mat inputted_image = app.GetImage();
   imgp::TableDetector table_detector;
-  table_detector.Recognize(inputted_image);
+  auto table = table_detector.Recognize(inputted_image);
+  OpenXLSX::XLDocument doc;
+  doc.create("./Demo01.xlsx");
+  auto wks = doc.workbook().worksheet("Sheet1");
+  for (size_t y = 0; y < table.getHeight(); y++) {
+    for (size_t x = 0; x < table.getWidth(); x++) {
+      wks.cell(x + 1, y + 1).value() = table.getCell(x, y);
+    }
+  }
+  doc.save();
+  doc.close();
+
   auto detected_cells = table_detector.DetectCells(inputted_image);
   int green = 0.0;
   for (auto cell : detected_cells) {
     cv::rectangle(inputted_image, cell, cv::Scalar(0, green, 0), 2);
-    green = (green + 4);
+    green = (green + 10) % 255;
   }
 
   cv::namedWindow("Test", cv::WINDOW_GUI_EXPANDED);
